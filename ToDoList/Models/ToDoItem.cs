40fd1ab113e090b8727c14c9b0f2c8e06af2c333
Kathelyn Gaioni <112026988+kathelyngaioni@users.noreply.github.com>
2023-05-14 22:48:01 +0000

    projeto final

---
+++ ToDoList/Models/ToDoItem.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoList.Models
+{
+    public class ToDoItem
+    {
+        public string Title {get;set;}
+        public bool IsDone {get;set;}
+        //public DateOnly data { get; set; }
+
+    }
+}
